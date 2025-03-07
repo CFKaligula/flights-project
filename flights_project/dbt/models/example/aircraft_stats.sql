30baffe3d5bb5910320dd51d480f58c61de6e8b2
C.F.K <casper.kole@gmail.com>
2025-03-07 12:12:42 +0100

    add dbt

---
+++ flights_project/dbt/models/example/aircraft_stats.sql
@@ -0,0 +1,16 @@
+﻿with at_stats as (
+    select
+    at.longDescription as aircrafttypeName,
+    count() as n_flights
+
+    from {{ source('default', 'flights') }} as f
+
+    join {{ source('default','aircrafttypes') }} as at
+    on f.aircraftTypeMain = at.iataMain and f.aircraftTypeSub = at.iataSub
+
+    group by at.longDescription
+
+)
+
+select *
+from at_stats
